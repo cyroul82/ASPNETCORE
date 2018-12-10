562771dd6fefe70b721983955850f486e4204a3a
Cyroul <crat@andeol.com>
2018-12-10 15:34:50 +0100

    Add artilce edit and form

--- Controllers/ArticleController.cs
+++ Controllers/ArticleController.cs
@@ -11,7 +11,23 @@ namespace AwesomeApp.Controllers
     public class ArticleController : Controller
     {
         public IActionResult Index(){
-            return View();
+            var list = new List<ArticleViewModel>();
+            var model = new ArticleViewModel {
+                 Id = 1,
+              Name = "DevOps" 
+            };
+            list.Add(model);
+            model = new ArticleViewModel {
+                 Id = 2,
+              Name = "Agilité" 
+            };
+            list.Add(model);
+            model = new ArticleViewModel {
+                Id = 3,
+                Name = "Scrum" 
+            };
+            list.Add(model);
+            return View(list);
         }
 
         [HttpGet]
@@ -21,6 +37,20 @@ namespace AwesomeApp.Controllers
               Name = "DevOps"  
             };
             return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id){
+            var model = new ArticleViewModel{
+              Id = 1,
+              Name = "DevOps"  
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ArticleViewModel article) {
+            return View("Detail", article);
         }
     }
 }
